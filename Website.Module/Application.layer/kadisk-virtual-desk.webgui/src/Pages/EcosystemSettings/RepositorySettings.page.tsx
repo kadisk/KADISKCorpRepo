98a6940928d852da029bb185cc664e1361ccee14
Kaio Cezar <kadisk.shark@gmail.com>
2025-01-28 16:47:03 -0300

    Style RepositorySettingsPage

--- Website.Module/Application.layer/kadisk-virtual-desk.webgui/src/Pages/EcosystemSettings/RepositorySettings.page.tsx
+++ Website.Module/Application.layer/kadisk-virtual-desk.webgui/src/Pages/EcosystemSettings/RepositorySettings.page.tsx
@@ -44,44 +44,7 @@ const INSTALLED_APPLICATIONS = [
 
 
 const RepositorySettings = () => {
-	return <div className="row row-cards">
-				<div className="col-12">
-					<div className="card">
-						<div className="card-header">
-							<div className="col">
-								<div className="page-pretitle">Repository Namespace</div>
-								<h2 className="page-title">EssentialRepo</h2>
-							</div>
-						</div>
-						<div className="card-body">
-							<div>
-								<div className="text-secondary">Installation Path</div>
-								<div>/home/kadisk/EcosystemData/repos/EssentialRepo</div>
-							</div>
-						</div>
-						<div className="card-body">
-							<h4>Source Data</h4>
-							<div className="datagrid">
-								<div className="datagrid-item">
-									<div className="datagrid-title">source type</div>
-									<div className="datagrid-content">LOCAL_FS</div>
-								</div>
-								<div className="datagrid-item">
-									<div className="datagrid-title">path</div>
-									<div className="datagrid-content">~/Workspaces/meta-platform-repo/repos/ecosystem-core-repository</div>
-								</div>
-							</div>
-						</div>
-						<div className="card-body">
-							<h4>Installed Applications</h4>
-							<Table list={INSTALLED_APPLICATIONS} columnsDefinition={columnsDefinition}/>
-						</div>
-					</div>
-				</div>
-			</div>
-}
 
-const RepositorySettingsPage = () => {
 
 	const columnsDefinition = {
 		"Executable": "executable",
@@ -90,11 +53,93 @@ const RepositorySettingsPage = () => {
 		"Supervisor Socket File Name": "supervisorSocketFileName"
 	}
 
-	return <DefaultPageWithTitle title="Repository Settings">
-				<div className="container-xl">
-					<RepositorySettings/>
+	return <div className="row row-cards">
+		<div className="col-12">
+			<div className="card">
+				<div className="card-header">
+					<div className="col">
+						<div className="page-pretitle">Repository Namespace</div>
+						<h2 className="page-title">EssentialRepo</h2>
+					</div>
 				</div>
-			</DefaultPageWithTitle>
-		}
+				<div className="card-body">
+					<div>
+						<div className="text-secondary">Installation Path</div>
+						<div>/home/kadisk/EcosystemData/repos/EssentialRepo</div>
+					</div>
+				</div>
+				<div className="card-body">
+					<h4>Source Data</h4>
+					<div className="datagrid">
+						<div className="datagrid-item">
+							<div className="datagrid-title">source type</div>
+							<div className="datagrid-content">LOCAL_FS</div>
+						</div>
+						<div className="datagrid-item">
+							<div className="datagrid-title">path</div>
+							<div className="datagrid-content">~/Workspaces/meta-platform-repo/repos/ecosystem-core-repository</div>
+						</div>
+					</div>
+				</div>
+				<div className="card-body">
+					<h4>Installed Applications</h4>
+					<Table list={INSTALLED_APPLICATIONS} columnsDefinition={columnsDefinition} />
+				</div>
+			</div>
+		</div>
+	</div>
+}
+
+const RepositorySettingsPage = () => {
+
+
+	return <DefaultPageWithTitle title="Repository Settings">
+		<div className="container-xl">
+			<div className="row row-cards">
+				<div className="col-12">
+					<div className="card">
+						<div className="table-responsive">
+							<table className="table table-vcenter table-mobile-md card-table">
+								<tbody>
+									<tr>
+										<td>
+											<div className="text-secondary">Repository Namespace</div>
+											<div><h3>EssentialRepo</h3></div>
+											<div className="card m-2 bg-primary-lt">
+												<div className="card-body p-2">
+												<h5 className="mb-1">Content source data</h5>
+												<div className="datagrid">
+													<div className="datagrid-item">
+														<div className="datagrid-title">source type</div>
+														<div className="datagrid-content">LOCAL_FS</div>
+													</div>
+													<div className="datagrid-item">
+														<div className="datagrid-title">path</div>
+														<div className="datagrid-content">~/Workspaces/meta-platform-repo/repos/ecosystem-core-repository</div>
+													</div>
+												</div>
+												</div>
+											</div>
+										</td>
+										<td className="text-end">
+											<button className="btn m-2">
+												<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-list-details"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M13 5h8" /><path d="M13 9h5" /><path d="M13 15h8" /><path d="M13 19h5" /><path d="M3 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M3 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /></svg>
+												show more details
+											</button>
+											<button className="btn btn-warning">
+												<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-refresh"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" /><path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" /></svg>
+												update repository content 
+											</button>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</DefaultPageWithTitle>
+}
 
 export default RepositorySettingsPage
