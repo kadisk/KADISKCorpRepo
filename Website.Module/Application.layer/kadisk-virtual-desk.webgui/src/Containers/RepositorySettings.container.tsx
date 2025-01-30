9939077c650c8828e06b6568b9557acbd5dcb7d6
Kaio Cezar <kadisk.shark@gmail.com>
2025-01-29 22:09:45 -0300

    RepositorySettingsContainer

--- Website.Module/Application.layer/kadisk-virtual-desk.webgui/src/Containers/RepositorySettings.container.tsx
+++ Website.Module/Application.layer/kadisk-virtual-desk.webgui/src/Containers/RepositorySettings.container.tsx
@@ -32,9 +32,9 @@ const RepositorySettings = ({ repositoryData, onGoBack }) => {
 						<h2 className="page-title">{repositoryNamespace}</h2>
 					</div>
                     <div className="col text-end">
-                        <button className="btn" onClick={onGoBack}>
+                        <button className="btn btn-outline-info" onClick={onGoBack}>
                             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M5 12l6 6" /><path d="M5 12l6 -6" /></svg>
-                            go back
+                            back
                         </button>
                     </div>
 				</div>
@@ -67,9 +67,7 @@ const RepositorySettings = ({ repositoryData, onGoBack }) => {
 }
 
 const RepositoriesTable = ({ list, onSettings }) => {
-
-
-
+    
     return <div className="table-responsive">
                 <table className="table table-vcenter table-mobile-md card-table">
                     <tbody>
@@ -103,7 +101,7 @@ const RepositoriesTable = ({ list, onSettings }) => {
                                                 </div>
                                             </td>
                                             <td className="text-end">
-                                                <button className="btn btn-secondary m-2" onClick={() => onSettings(repositoryData)}>
+                                                <button className="btn btn-outline-info m-2" onClick={() => onSettings(repositoryData)}>
                                                     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-adjustments-cog"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 10a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M6 4v4" /><path d="M6 12v8" /><path d="M13.199 14.399a2 2 0 1 0 -1.199 3.601" /><path d="M12 4v10" /><path d="M12 18v2" /><path d="M16 7a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M18 4v1" /><path d="M18 9v2.5" /><path d="M19.001 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M19.001 15.5v1.5" /><path d="M19.001 21v1.5" /><path d="M22.032 17.25l-1.299 .75" /><path d="M17.27 20l-1.3 .75" /><path d="M15.97 17.25l1.3 .75" /><path d="M20.733 20l1.3 .75" /></svg>
                                                     settings
                                                 </button>
@@ -124,8 +122,6 @@ const RepositorySettingsContainer = ({ HTTPServerManager }) => {
 
     const [ repositoryList, setRepositoryList ] = useState<any[]>()
     const [ repositoryDataSettings, setRepositoryDataSettings ] = useState<any>()
-
-    console.log(repositoryList)
 
     useEffect(() => {
         fetchDefaultParameters()
