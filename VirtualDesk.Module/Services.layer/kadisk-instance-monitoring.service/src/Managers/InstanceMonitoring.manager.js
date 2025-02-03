43b3c55a266a827c7e6959d6b437d8a872f2dac9
Kaio Cezar <kadisk.shark@gmail.com>
2025-02-03 15:11:55 -0300

    module.exports = InstanceMonitoringManager

--- VirtualDesk.Module/Services.layer/kadisk-instance-monitoring.service/src/Managers/InstanceMonitoring.manager.js
+++ VirtualDesk.Module/Services.layer/kadisk-instance-monitoring.service/src/Managers/InstanceMonitoring.manager.js
@@ -3,13 +3,39 @@ const { Sequelize, DataTypes } = require('sequelize')
 
 const EventEmitter = require('node:events')
 const { resolve } = require("path")
-const colors = require("colors")
+//const colors = require("colors")
 
 const AreArraysEqual = require("../Utils/AreArraysEqual")
 
+const InstanceConnectionStatus = Object.freeze({
+    CONNECTING: "CONNECTING",
+    CONNECTED : "CONNECTED",
+    UNAVAILABLE: "UNAVAILABLE"
+})
+
 const CreateInstanceSocketHandlerManager = require("../Helpers/CreateInstanceSocketHandlerManager")
 
+
+const CreateConnectionClientHandler = () => {
+
+    let clients = {}
+
+    const AddNewClient = (socketFileId, communicationClient) => {
+        clients[socketFileId] = communicationClient
+    }
+
+
+    const GetClient = (socketFileId) => clients[socketFileId]
+
+    return {
+        AddNewClient,
+        GetClient
+    }
+}
+
 const InstanceMonitoringManager = (params) => {
+
+    const ConnectionClientHandler = CreateConnectionClientHandler()
 
     const monitoringUUID = randomUUID()
 
@@ -45,7 +71,7 @@ const InstanceMonitoringManager = (params) => {
     })
 
 
-    const InstanceConnectionEventsModel = sequelize.define("InstanceConnectionEvents", {
+    const ConnectionLogsModel = sequelize.define("ConnectionLogs", {
         id: {
             type: DataTypes.INTEGER,
             autoIncrement: true,
@@ -66,17 +92,17 @@ const InstanceMonitoringManager = (params) => {
         }
     })
 
-    SocketFileModel.hasMany(InstanceConnectionEventsModel, {
+    SocketFileModel.hasMany(ConnectionLogsModel, {
         foreignKey: 'socketFileId',
         onDelete: 'CASCADE'
     })
     
-    InstanceConnectionEventsModel.belongsTo(SocketFileModel, {
+    ConnectionLogsModel.belongsTo(SocketFileModel, {
         foreignKey: 'socketFileId'
     })
 
 
-    const WatchSocketDirectory         = supervisorLib.require("WatchSocketDirectory")
+    //const WatchSocketDirectory         = supervisorLib.require("WatchSocketDirectory")
     const ListSocketFilesName          = supervisorLib.require("ListSocketFilesName")
     const CreateCommunicationInterface = supervisorLib.require("CreateCommunicationInterface")
     const ReadJsonFile                 = jsonFileUtilitiesLib.require("ReadJsonFile")
@@ -85,12 +111,12 @@ const InstanceMonitoringManager = (params) => {
     let supervisorSocketsDirPath = undefined
 
     const {
-        Overview,
-        TryInitializeSocketMonitoring,
-        InitializeSocketMonitoring,
-        GetMonitoringKeysReady,
+        //Overview,
+        //TryInitializeSocketMonitoring,
+        //InitializeSocketMonitoring,
+        //GetMonitoringKeysReady,
         GetSocketMonitoringState,
-        AddEventListener
+        //AddEventListener
     } = CreateInstanceSocketHandlerManager({
         helpers:{
             CreateCommunicationInterface,
@@ -98,7 +124,7 @@ const InstanceMonitoringManager = (params) => {
         }
     })
 
-    const _CreateHandlerSocketDirectoryChange = () => {
+    /*const _CreateHandlerSocketDirectoryChange = () => {
         
         let socketFileNameList = []
 
@@ -120,7 +146,7 @@ const InstanceMonitoringManager = (params) => {
         }
         
         return __HandlerSocketDirectoryChange
-    }
+    }*/
 
     const _GetSocketFilePath = (socketFileName) => resolve(supervisorSocketsDirPath, socketFileName)
 
@@ -138,6 +164,29 @@ const InstanceMonitoringManager = (params) => {
         await SocketFileModel.bulkCreate(listForCreate)
     }
 
+    const RecordConnectionStatusChange = (socketFileId, status) => {
+        ConnectionLogsModel.create({ socketFileId, status })
+    }
+
+    const ConnectToInstance = async (socketFileId, socketFilePath) => {
+        await RecordConnectionStatusChange(socketFileId, InstanceConnectionStatus.CONNECTING)
+        try{
+            const communicationClient = await CreateCommunicationInterface(socketFilePath)
+            ConnectionClientHandler.AddNewClient(socketFileId, communicationClient)
+            await RecordConnectionStatusChange(socketFileId, InstanceConnectionStatus.CONNECTED)
+        }catch(e){
+            await RecordConnectionStatusChange(socketFileId, InstanceConnectionStatus.UNAVAILABLE)
+        }
+    }
+
+    const _TryConnectInAllInstances = async () => {
+        const socketFileList = await _GetAllSocketFile()
+
+        socketFileList
+        .forEach(({id, socketFilePath}) => ConnectToInstance(id, socketFilePath))
+
+    }
+
     const _Start = async () => {
 
         await sequelize.authenticate()
@@ -148,6 +197,8 @@ const InstanceMonitoringManager = (params) => {
         const socketFilePaths = _ConvertSocketFileNamesToFilePaths(socketFileNames)
 
         await _RegisterAllSocketFiles(socketFilePaths)
+
+        _TryConnectInAllInstances()
 
         /*socketFileNames.forEach((socketFileName) => InitializeSocketMonitoring(_GetSocketFilePath(socketFileName)))
 
@@ -168,7 +219,7 @@ const InstanceMonitoringManager = (params) => {
         return socketsDirPath
     }
 
-    const OverviewChangeListener = AddEventListener
+    //const OverviewChangeListener = AddEventListener
 
     const _GetConnectionClient = (monitoringStateKey) => {
         const socketMonitoringState = GetSocketMonitoringState(monitoringStateKey)
@@ -181,17 +232,67 @@ const InstanceMonitoringManager = (params) => {
         const responseData = await communicationClient[fname](fArgs)
         return responseData
     }
-
+/*
     const ListInstanceTasks     = async (monitoringStateKey) =>           await _CallRPC(monitoringStateKey, "ListTasks")
     const GetTaskInformation    = async ({monitoringStateKey, taskId}) => await _CallRPC(monitoringStateKey, "GetTask", taskId)
     const GetStartupArguments   = async (monitoringStateKey) =>           await _CallRPC(monitoringStateKey, "GetStartupArguments")
-    const GetProcessInformation = async (monitoringStateKey) =>           await _CallRPC(monitoringStateKey, "GetProcessInformation")
+    const GetProcessInformation = async (monitoringStateKey) =>           await _CallRPC(monitoringStateKey, "GetProcessInformation")*/
 
+    const _GetAllSocketFile = () => 
+        SocketFileModel
+            .findAll({
+                where: {
+                    monitoringUUID
+                }
+            })
+
+
+    const _GetConnectionStatus = (socketFileIds) => 
+        ConnectionLogsModel.findAll({
+            where: {
+                socketFileId: socketFileIds
+            },
+            attributes: [
+                'id',
+                'status',
+                'socketFileId',
+                'createdAt'
+            ],
+            include: [
+                {
+                    model: SocketFileModel,
+                    as: 'SocketFile'
+                }
+            ],
+            order: [['createdAt', 'DESC']],
+            group: ['socketFileId']
+        })
 
     const GetInstancesOverview = async () => {
         try {
-            const instancesOverview = await SocketFileModel.findAll()
-            return instancesOverview
+            const socketFileList = await _GetAllSocketFile()
+            const socketFileIds = socketFileList.map(({id})=> id)
+            const connectionStatusLogs = await _GetConnectionStatus(socketFileIds)
+
+            const overview = connectionStatusLogs
+            .map((rowData) => {
+                const {
+                    socketFileId,
+                    createdAt,
+                    status,
+                    SocketFile:{
+                        socketFilePath
+                    }
+                } = rowData
+                return {
+                    socketFileId,
+                    socketFilePath,
+                    createdAt,
+                    status
+                }
+            })
+
+            return overview
         } catch (error) {
             console.error('Error listing history:', error)
             throw error
@@ -199,13 +300,13 @@ const InstanceMonitoringManager = (params) => {
     }
 
     const monitoringObject = {
-        OverviewChangeListener,
-        GetMonitoringKeysReady,
+        //OverviewChangeListener,
+        //GetMonitoringKeysReady,
         GetInstancesOverview,
-        ListInstanceTasks,
-        GetTaskInformation,
-        GetStartupArguments,
-        GetProcessInformation
+       // ListInstanceTasks,
+      //  GetTaskInformation,
+       // GetStartupArguments,
+       // GetProcessInformation
     }
         
     _Start()
--- VirtualDesk.Module/UserInterfaces.layer/kadisk-virtual-desk.webgui/src/Containers/InstanceMonitoring.container.tsx
+++ VirtualDesk.Module/UserInterfaces.layer/kadisk-virtual-desk.webgui/src/Containers/InstanceMonitoring.container.tsx
@@ -1,81 +1,63 @@
 import * as React from "react"
+import { useEffect, useState } from "react"
+import { connect } from "react-redux"
+import { bindActionCreators } from "redux"
 
-const InstanceMonitoringContainer = () => {
+import GetAPI from "../Utils/GetAPI"
+
+const InstanceMonitoringContainer = ({ HTTPServerManager }) => {
+
+    const [ instanceOverview, setInstanceOverview ] = useState<any[]>()
+
+    useEffect(() => {
+        fetchInstanceOverview()
+    }, [])
+
+    const _GetInstanceMonitoringAPI = () =>
+        GetAPI({
+            apiName: "InstanceMonitoring",
+            serverManagerInformation: HTTPServerManager
+        })
+
+    const fetchInstanceOverview = async () => {
+        const api = _GetInstanceMonitoringAPI()
+        const response = await api.GetInstancesOverview()
+        setInstanceOverview(response.data)
+    }
+
     return <div className="container-xl">
                 <div className="col-12">
                     <div className="card">
                         <div className="card-header">
-                            <h3 className="card-title">Last updates</h3>
+                            <h3 className="card-title">Monitored Instances</h3>
                         </div>
                         <div className="list-group list-group-flush list-group-hoverable">
-                            <div className="list-group-item">
-                                <div className="row align-items-center">
-                                    <div className="col-auto"><span className="status-dot status-dot-animated bg-red d-block"></span></div>
-                                    <div className="col text-truncate">
-                                        <a href="#" className="text-body d-block">Example 1</a>
-                                        <div className="d-block text-secondary text-truncate mt-n1">
-                                            Change deprecated html tags to text decoration classes (#29604)
-                                        </div>
-                                    </div>
-                                    <div className="col-auto">
-                                        <a href="#" className="list-group-item-actions">
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon text-muted" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="list-group-item">
-                                <div className="row align-items-center">
-                                    <div className="col-auto"><span className="status-dot d-block"></span></div>
-                                    <div className="col text-truncate">
-                                        <a href="#" className="text-body d-block">Example 2</a>
-                                        <div className="d-block text-secondary text-truncate mt-n1">
-                                            justify-content:between ⇒ justify-content:space-between (#29734)
-                                        </div>
-                                    </div>
-                                    <div className="col-auto">
-                                        <a href="#" className="list-group-item-actions show">
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon text-yellow" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="list-group-item">
-                                <div className="row align-items-center">
-                                    <div className="col-auto"><span className="status-dot d-block"></span></div>
-                                    <div className="col text-truncate">
-                                        <a href="#" className="text-body d-block">Example 3</a>
-                                        <div className="d-block text-secondary text-truncate mt-n1">
-                                            Update change-version.js (#29736)
-                                        </div>
-                                    </div>
-                                    <div className="col-auto">
-                                        <a href="#" className="list-group-item-actions">
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon text-muted" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="list-group-item">
-                                <div className="row align-items-center">
-                                    <div className="col-auto"><span className="status-dot status-dot-animated bg-green d-block"></span></div>
-                                    <div className="col text-truncate">
-                                        <a href="#" className="text-body d-block">Example 4</a>
-                                        <div className="d-block text-secondary text-truncate mt-n1">
-                                            Regenerate package-lock.json (#29730)
-                                        </div>
-                                    </div>
-                                    <div className="col-auto">
-                                        <a href="#" className="list-group-item-actions">
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon text-muted" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+                            {
+
+                                instanceOverview
+                                && instanceOverview
+                                .map(({ socketFilePath, status, createdAt }) => {
+                                    return <div className="list-group-item">
+                                                <div className="row align-items-center">
+                                                    <div className="col-auto"><span className="badge bg-green text-green-fg">{status}</span></div>
+                                                    <div className="col text-truncate">
+                                                        <a href="#" className="text-body d-block">{socketFilePath}</a>
+                                                        <div className="d-block text-secondary text-truncate mt-n1">
+                                                            {createdAt}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                })
+                            }
                         </div>
                     </div>
                 </div>
             </div>
 }
 
-export default InstanceMonitoringContainer
+const mapDispatchToProps = (dispatch: any) => bindActionCreators({}, dispatch)
+
+const mapStateToProps = ({ HTTPServerManager }: any) => ({ HTTPServerManager })
+
+export default connect(mapStateToProps, mapDispatchToProps)(InstanceMonitoringContainer)
