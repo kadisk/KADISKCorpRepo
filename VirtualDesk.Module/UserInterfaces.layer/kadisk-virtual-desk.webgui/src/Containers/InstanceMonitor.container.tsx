65bcb8eafaab9b51e801d078f8f24603501a04e5
Kaio Cezar <kadisk.shark@gmail.com>
2025-02-04 13:40:34 -0300

    InstanceMonitorContainer

--- VirtualDesk.Module/UserInterfaces.layer/kadisk-virtual-desk.webgui/src/Containers/InstanceMonitor.container.tsx
+++ VirtualDesk.Module/UserInterfaces.layer/kadisk-virtual-desk.webgui/src/Containers/InstanceMonitor.container.tsx
@@ -7,6 +7,8 @@ import GetAPI from "../Utils/GetAPI"
 
 const InstanceMonitorContainer = ({ HTTPServerManager, socketFileId }) => {
 
+    const [ instanceMonitorData, setInstanceMonitorData] = useState<any>()
+
     useEffect(() => {
         fetchInstanceMonitorData()
     }, [])
@@ -20,114 +22,89 @@ const InstanceMonitorContainer = ({ HTTPServerManager, socketFileId }) => {
     const fetchInstanceMonitorData = async () => {
         const api = _GetInstanceMonitoringAPI()
         const response = await api.GetInstanceMonitorData({socketFileId})
+        setInstanceMonitorData(response.data)
     }
 
     return <>
-        <div className="page-header d-print-none">
-            <div className="container">
-                <div className="row g-3">
-                    <div className="col">
-                        <h2 className="page-title">
-                            tabler.io/icons
-                        </h2>
-                        <div className="text-secondary">
-                            <ul className="list-inline list-inline-dots mb-0">
-                                <li className="list-inline-item"><span className="text-green">Up</span></li>
-                                <li className="list-inline-item">Checked every 3 minutes</li>
-                            </ul>
+        {
+            instanceMonitorData
+            && <div className="page-header d-print-none">
+                    <div className="container">
+                        <div className="row g-3">
+                            <div className="col">
+                                <h2 className="page-title">
+                                    {instanceMonitorData.startupArguments.packagePath}
+                                </h2>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+        }
         <div className="page-body">
             <div className="container-xl">
-                <div className="row row-cards">
-                    <div className="col-md-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <div className="subheader">Currently up for</div>
-                                <div className="h3 m-0">14 days 2 hours 54 mins 34 seconds</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <div className="subheader">Last checked at</div>
-                                <div className="h3 m-0">27 seconds ago</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <div className="subheader">Incidents</div>
-                                <div className="h3 m-0">3</div>
-                            </div>
-                        </div>
-                    </div>
+                {
+                    instanceMonitorData
+                    && <div className="row row-cards">
+                            {
+                                instanceMonitorData?.processInformation
+                                && <div className="col-md-2">
+                                        <div className="card">
+                                            <div className="card-body">
+                                                <div className="subheader mb-2">Process Information</div>
 
-                    <div className="col-12">
-                        <div className="card">
-                            <div className="card-table table-responsive">
-                                <table className="table">
-                                    <thead>
-                                        <tr>
-                                            <th>Time period</th>
-                                            <th>Availability</th>
-                                            <th>Downtime</th>
-                                            <th>Incidents</th>
-                                            <th>Longest incident</th>
-                                            <th>Avg. incident</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody><tr>
-                                        <td>Today</td>
-                                        <td>98.9533%</td>
-                                        <td>1 minute</td>
-                                        <td>1</td>
-                                        <td>1 minute</td>
-                                        <td>1 minute</td>
-                                    </tr>
-                                        <tr>
-                                            <td>Last 7 days</td>
-                                            <td>98.9533%</td>
-                                            <td>1 minute</td>
-                                            <td>1</td>
-                                            <td>1 minute</td>
-                                            <td>1 minute</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Last 30 days</td>
-                                            <td>98.9533%</td>
-                                            <td>1 minute</td>
-                                            <td>1</td>
-                                            <td>1 minute</td>
-                                            <td>1 minute</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Last 365 days</td>
-                                            <td>98.9533%</td>
-                                            <td>1 minute</td>
-                                            <td>1</td>
-                                            <td>1 minute</td>
-                                            <td>1 minute</td>
-                                        </tr>
-                                        <tr>
-                                            <td>All time</td>
-                                            <td>98.9533%</td>
-                                            <td>1 minute</td>
-                                            <td>1</td>
-                                            <td>1 minute</td>
-                                            <td>1 minute</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                                                <div>PID: <strong>{instanceMonitorData.processInformation.pid}</strong></div>
+                                                <div>Platform: <strong>{instanceMonitorData.processInformation.platform}</strong></div>
+                                                <div>Architecture: <strong>{instanceMonitorData.processInformation.arch}</strong></div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                            }
+
+                            {
+                                instanceMonitorData?.instanceTasks
+                                && <div className="col-12">
+                                        <div className="card">
+                                            <div className="card-table table-responsive">
+                                                <table className="table">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>STATUS</th>
+                                                            <th>TID</th>
+                                                            <th>PTID</th>
+                                                            <th>Object Loader Type</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        {
+                                                            instanceMonitorData.instanceTasks
+                                                            .map((taskData) => {
+
+                                                                const {
+                                                                    taskId,
+                                                                    pTaskId,
+                                                                    objectLoaderType,
+                                                                    status
+                                                                } = taskData
+
+                                                                return <tr>
+                                                                            <td>{status}</td>
+                                                                            <td>{taskId}</td>
+                                                                            <td>{pTaskId}</td>
+                                                                            <td>{objectLoaderType}</td>
+                                                                        </tr>
+                                                            })
+                                                        }
+                                                        
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                            }
                         </div>
-                    </div>
-                </div>
+                }
+                
             </div>
         </div>
     </>
