b4e5d4cd263a925311b5aa5fbddfa954aaac5bc6
Kaio Cezar <kadisk.shark@gmail.com>
2025-02-06 20:54:19 -0300

    CreateMonitoringDatabaseHandler

--- VirtualDesk.Module/Services.layer/kadisk-instance-monitoring.service/src/Managers/InstanceMonitoring.manager.js
+++ VirtualDesk.Module/Services.layer/kadisk-instance-monitoring.service/src/Managers/InstanceMonitoring.manager.js
@@ -1,7 +1,4 @@
 const { randomUUID } = require("crypto")
-const { Sequelize, DataTypes } = require('sequelize')
-
-const EventEmitter = require('node:events')
 const { resolve } = require("path")
 
 const InstanceConnectionStatus = Object.freeze({
@@ -9,6 +6,8 @@ const InstanceConnectionStatus = Object.freeze({
     CONNECTED : "CONNECTED",
     UNAVAILABLE: "UNAVAILABLE"
 })
+
+const CreateMonitoringDatabaseHandler = require("../Helpers/CreateMonitoringDatabaseHandler")
 
 const CreateConnectionClientHandler = () => {
 
@@ -28,10 +27,6 @@ const CreateConnectionClientHandler = () => {
 
 const InstanceMonitoringManager = (params) => {
 
-    const ConnectionClientHandler = CreateConnectionClientHandler()
-
-    const monitoringUUID = randomUUID()
-
     const {
         ecosystemdataHandlerService,
         ecosystemDefaultsFileRelativePath,
@@ -41,96 +36,10 @@ const InstanceMonitoringManager = (params) => {
         onReady 
     } = params
 
-    const sequelize = new Sequelize({
-            dialect: 'sqlite',
-            storage: storageFilePath
-    })
+    const monitoringUUID = randomUUID()
 
-   
-    const SocketFileModel = sequelize.define('SocketFile', {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        monitoringUUID: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        socketFilePath: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    })
-
-
-    const ConnectionLogsModel = sequelize.define("ConnectionLogs", {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        status: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        socketFileId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: SocketFileModel,
-                key: 'id'
-            },
-            onDelete: "CASCADE"
-        }
-    })
-
-    const InstanceInformationModel = sequelize.define("InstanceInformation", {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        pid: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        platform: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        arch: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        socketFileId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: SocketFileModel,
-                key: 'id'
-            },
-            onDelete: "CASCADE"
-        }
-    })
-
-    SocketFileModel.hasMany(ConnectionLogsModel, {
-        foreignKey: 'socketFileId',
-        onDelete: 'CASCADE'
-    })
-    
-    ConnectionLogsModel.belongsTo(SocketFileModel, {
-        foreignKey: 'socketFileId'
-    })
-
-    SocketFileModel.hasMany(InstanceInformationModel, {
-        foreignKey: 'socketFileId',
-        onDelete: 'CASCADE'
-    })
-    
-    InstanceInformationModel.belongsTo(SocketFileModel, {
-        foreignKey: 'socketFileId'
-    })
+    const MonitoringDatabaseHandler = CreateMonitoringDatabaseHandler({ monitoringUUID, storageFilePath })
+    const ConnectionClientHandler = CreateConnectionClientHandler()
 
     //const WatchSocketDirectory       = supervisorLib.require("WatchSocketDirectory")
     const ListSocketFilesName          = supervisorLib.require("ListSocketFilesName")
@@ -140,63 +49,24 @@ const InstanceMonitoringManager = (params) => {
     const ecosystemDefaultFilePath = resolve(ecosystemdataHandlerService.GetEcosystemDataPath(), ecosystemDefaultsFileRelativePath)
     let supervisorSocketsDirPath = undefined
 
-    /*const _CreateHandlerSocketDirectoryChange = () => {
-        
-        let socketFileNameList = []
-
-        const __ChangeList = (newList) => {
-            socketFileNameList = newList
-            NotifyEvent({
-                origin: "InstanceMonitoringManager",
-                type:"message",
-                content: `A lista de sockets foi atualizada para ${colors.bold(newList.join(", "))}`
-            })
-        }
-
-        const __HandlerSocketDirectoryChange = (newSocketFileNameList) => {
-            if(!AreArraysEqual(newSocketFileNameList, socketFileNameList)){
-                __ChangeList(newSocketFileNameList)
-                newSocketFileNameList
-                .forEach((socketFileName) => TryInitializeSocketMonitoring(_GetSocketFilePath(socketFileName)))
-            }
-        }
-        
-        return __HandlerSocketDirectoryChange
-    }*/
-
     const _GetSocketFilePath = (socketFileName) => resolve(supervisorSocketsDirPath, socketFileName)
 
     const _ConvertSocketFileNamesToFilePaths = (socketFileNames) => socketFileNames.map(_GetSocketFilePath)
 
-    const _RegisterAllSocketFiles = async (socketFilePaths) => {
-
-        const listForCreate = socketFilePaths.map((socketFilePath) => {
-            return {
-                socketFilePath,
-                monitoringUUID
-            }
-        })
-
-        await SocketFileModel.bulkCreate(listForCreate)
-    }
-
-    const RecordConnectionStatusChange = (socketFileId, status) => {
-        ConnectionLogsModel.create({ socketFileId, status })
-    }
 
     const ConnectToInstance = async (socketFileId, socketFilePath) => {
-        await RecordConnectionStatusChange(socketFileId, InstanceConnectionStatus.CONNECTING)
+        await MonitoringDatabaseHandler.RegisterConnectionStatusChange(socketFileId, InstanceConnectionStatus.CONNECTING)
         try{
             const communicationClient = await CreateCommunicationInterface(socketFilePath)
             ConnectionClientHandler.AddNewClient(socketFileId, communicationClient)
-            await RecordConnectionStatusChange(socketFileId, InstanceConnectionStatus.CONNECTED)
+            await MonitoringDatabaseHandler.RegisterConnectionStatusChange(socketFileId, InstanceConnectionStatus.CONNECTED)
         }catch(e){
-            await RecordConnectionStatusChange(socketFileId, InstanceConnectionStatus.UNAVAILABLE)
+            await MonitoringDatabaseHandler.RegisterConnectionStatusChange(socketFileId, InstanceConnectionStatus.UNAVAILABLE)
         }
     }
 
     const _TryConnectInAllInstances = async () => {
-        const socketFileList = await _GetAllSocketFile()
+        const socketFileList = await MonitoringDatabaseHandler.GetAllSocketFile()
 
         socketFileList
         .forEach(({id, socketFilePath}) => ConnectToInstance(id, socketFilePath))
@@ -205,21 +75,17 @@ const InstanceMonitoringManager = (params) => {
 
     const _Start = async () => {
 
-        await sequelize.authenticate()
-        await sequelize.sync()
+        await MonitoringDatabaseHandler.Initialize()
 
         const socketsDirPath = await _GetSocketsDirPath()
         const socketFileNames = await ListSocketFilesName(socketsDirPath)
         const socketFilePaths = _ConvertSocketFileNamesToFilePaths(socketFileNames)
 
-        await _RegisterAllSocketFiles(socketFilePaths)
+        await MonitoringDatabaseHandler.RegisterAllSocketFiles(socketFilePaths)
 
         _TryConnectInAllInstances()
 
-        /*socketFileNames.forEach((socketFileName) => InitializeSocketMonitoring(_GetSocketFilePath(socketFileName)))
-
-        const __HandlerSocketDirectoryChange = _CreateHandlerSocketDirectoryChange()
-
+        /*
         WatchSocketDirectory({
             directoryPath: supervisorSocketsDirPath, 
             onChangeSocketFileList: __HandlerSocketDirectoryChange
@@ -240,48 +106,19 @@ const InstanceMonitoringManager = (params) => {
         const responseData = await communicationClient[fname](fArgs)
         return responseData
     }
-/*
-    const ListInstanceTasks     = async (monitoringStateKey) =>           await _CallRPC(monitoringStateKey, "ListTasks")
-    const GetTaskInformation    = async ({monitoringStateKey, taskId}) => await _CallRPC(monitoringStateKey, "GetTask", taskId)*/
+    
+
+    //const GetTaskInformation    = async (socketFileId) => await _CallRPC(socketFileId, "GetTask", taskId)
     const ListInstanceTasks     = async (socketFileId) => await _CallRPC(socketFileId, "ListTasks")
     const GetStartupArguments   = async (socketFileId) => await _CallRPC(socketFileId, "GetStartupArguments")
     const GetProcessInformation = async (socketFileId) => await _CallRPC(socketFileId, "GetProcessInformation")
 
-    const _GetAllSocketFile = () => 
-        SocketFileModel
-            .findAll({
-                where: {
-                    monitoringUUID
-                }
-            })
-
-
-    const _GetConnectionStatus = (socketFileIds) => 
-        ConnectionLogsModel.findAll({
-            where: {
-                socketFileId: socketFileIds
-            },
-            attributes: [
-                'id',
-                'status',
-                'socketFileId',
-                'createdAt'
-            ],
-            include: [
-                {
-                    model: SocketFileModel,
-                    as: 'SocketFile'
-                }
-            ],
-            order: [['createdAt', 'DESC']],
-            group: ['socketFileId']
-        })
 
     const GetInstancesOverview = async () => {
         try {
-            const socketFileList = await _GetAllSocketFile()
+            const socketFileList = await MonitoringDatabaseHandler.GetAllSocketFile()
             const socketFileIds = socketFileList.map(({id})=> id)
-            const connectionStatusLogs = await _GetConnectionStatus(socketFileIds)
+            const connectionStatusLogs = await MonitoringDatabaseHandler.GetConnectionStatus(socketFileIds)
 
             const overview = connectionStatusLogs
             .map((rowData) => {
