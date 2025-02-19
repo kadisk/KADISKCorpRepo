487468ff89595915f2f1a8dddb5793098785a736
Kaio Cezar <kadisk.shark@gmail.com>
2025-02-19 20:26:47 -0300

    ajuste

--- VirtualDesk.Module/UserInterfaces.layer/kadisk-virtual-desk.webgui/src/Containers/RepositoryEditor.container.tsx
+++ VirtualDesk.Module/UserInterfaces.layer/kadisk-virtual-desk.webgui/src/Containers/RepositoryEditor.container.tsx
@@ -41,52 +41,51 @@ const RepositoryEditorContainer = ({ repositoryId, HTTPServerManager }) => {
     }
 
     const renderHierarchy = (items) => {
-        return (
-            <ul className="list-group">
-                {items.map((item) => (
-                    <li key={item.id} className="list-group-item border-0 p-1 cursor-pointer">
-                        <div className="d-flex align-items-center">
-                            <span className="me-2 text-primary">
-                                <i className="ti ti-folder" />
-                            </span>
-                            <span className={`badge me-2 ${getClassNameColor(item.itemType)}`}>{item.itemType}</span>
-                            <span className="fw-bold fs-3">{item.itemName}</span>
-                        </div>
-                        {item.children && item.children.length > 0 && (
-                            <div className="ms-4 mt-2 border-start ps-3">
-                                {renderHierarchy(item.children)}
+        return <ul className="list-group">
+                    {items.map((item) => (
+                        <li key={item.id} className="list-group-item border-0 p-0 cursor-pointer">
+                            <div className="d-flex align-items-center">
+                                <span>{item.itemName} <span className="text-secondary">{item.itemType}</span></span>
                             </div>
-                        )}
-                    </li>
-                ))}
-            </ul>
-        )
+                            {item.children && item.children.length > 0 && (
+                                <div className="ms-2 mt-1 border-start ps-2">
+                                    {renderHierarchy(item.children)}
+                                </div>
+                            )}
+                        </li>
+                    ))}
+                </ul>
     }
 
     return (
         <div className="d-flex" style={{ height: "100vh", overflow: "hidden" }}>
             <aside className="navbar navbar-vertical navbar-expand-lg flex-shrink-0 d-flex" style={{ height: "100%", overflowY: "auto", minWidth: "max-content", display: "flex", flexDirection: "column", position: "relative" }}>
                 <div className="container-fluid">
-                    <div className="navbar-brand">
-                        <a href=".">
-                            <img src={logoVirtualDesk2} width={200} />
-                        </a>
-                    </div>
-                    {repositoryHierarchy.length > 0 ? (
-                        renderHierarchy(repositoryHierarchy)
-                    ) : (
-                        <div className="text-center py-4">
-                            <div className="spinner-border text-primary" role="status">
-                                <span className="visually-hidden">Loading...</span>
+                    
+                        <div className="col ps-3 mb-2 d-flex align-items-center border-bottom">
+                            <div>
+                                <div className="page-pretitle">Repository Editor</div>
+                                <h2 className="page-title" style={{color:"black"}}>Essential</h2>
                             </div>
                         </div>
-                    )}
+
+                    <div className="ms-3 me-3">
+                    {
+                        repositoryHierarchy.length > 0 
+                        ? renderHierarchy(repositoryHierarchy) 
+                        : <div className="text-center py-4">
+                                <div className="spinner-border text-primary" role="status">
+                                    <span className="visually-hidden">Loading...</span>
+                                </div>
+                            </div>
+                    }
+                    </div>
                 </div>
             </aside>
             <div className="page-wrapper flex-grow-1 d-flex flex-column" style={{ overflowY: "auto", flex: 1, display: "flex", minWidth: 0, marginLeft: 0 }}>
                 <div className="container-fluid flex-grow-1 d-flex p-0">
                     <div className="row flex-grow-1 m-0">
-                        <div className="col-md-4">
+                        <div className="col-12">
                             <div className="card">
                                 <div className="card-body p-3">XPTOP</div>
                             </div>
