f3bfc735e8539eeec3d726758d0de33c8276a10d
Kaio Cezar <kadisk.shark@gmail.com>
2025-03-16 20:14:25 -0300

    UserTable

--- VirtualDesk.Module/UserInterfaces.layer/kadisk-virtual-desk.webgui/src/Containers/UserAdministration.container/User.table.tsx
+++ VirtualDesk.Module/UserInterfaces.layer/kadisk-virtual-desk.webgui/src/Containers/UserAdministration.container/User.table.tsx
@@ -1,6 +1,50 @@
 import * as React from "react"
 
-import Table from "../../Components/Table"
+type TableProps = {
+    allTdClassName?:string
+    columnsDefinition:any
+    list:any[]
+}
+
+const Table = ({
+    allTdClassName,
+    columnsDefinition,
+    list
+}: TableProps) => {
+
+    const columnsName = Object.keys(columnsDefinition)
+    const columnsProperty = Object.values(columnsDefinition)
+
+    return <table className="table table-vcenter card-table">
+                <thead>
+                    <tr>
+                        {
+                            columnsName
+                            .map((columnName) => <th>{columnName}</th>)
+                        }
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        (list || [])
+                        .map((data) =>
+                            <tr>
+                                {
+                                    columnsProperty
+                                    .map((property:any) => <td className={allTdClassName}>{data[property]}</td>)
+                                }
+                                <td>
+                                    <div className="btn-list flex-nowrap">
+                                        <button className="btn btn-1"> Edit </button>
+                                    </div>
+                                </td>
+                            </tr>)
+                    }
+                </tbody>
+            </table>
+}
+
 
 const UserTable = ({ users }) => {
 
