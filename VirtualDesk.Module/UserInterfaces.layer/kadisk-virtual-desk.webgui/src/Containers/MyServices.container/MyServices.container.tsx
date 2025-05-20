cb4c4e87181cd3b9c3578926de26874eecddca8f
Kaio Cezar <kadisk.shark@gmail.com>
2025-05-20 19:14:18 -0300

    Service Settings

--- VirtualDesk.Module/UserInterfaces.layer/kadisk-virtual-desk.webgui/src/Containers/MyServices.container/MyServices.container.tsx
+++ VirtualDesk.Module/UserInterfaces.layer/kadisk-virtual-desk.webgui/src/Containers/MyServices.container/MyServices.container.tsx
@@ -11,6 +11,8 @@ import ServiceProvisioningModal from "./ServiceProvisioning.modal"
 import RepositoriesManagerModal from "./RepositoriesManager.modal"
 import ImportingModal from "./Importing.modal"
 
+import ServiceSettingsPanel from "./ServiceSettings.panel"
+
 const DEFAULT_MODE              = Symbol()
 const IMPORT_SELECT_MODE        = Symbol()
 const IMPORTING_MODE            = Symbol()
@@ -18,7 +20,13 @@ const NO_REPOSITORIES_MODE      = Symbol()
 const LOADING_MODE              = Symbol()
 const SERVICE_PROVISIONING_MODE = Symbol()
 const REPOSITORIES_MANAGER_MODE = Symbol()
+const SERVICE_SETINGS_MODE      = Symbol()
 
+
+const START_ICON = <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-player-play"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 4v16l13 -8z" /></svg>
+const RESTART_ICON = <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-refresh"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" /><path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" /></svg>
+const STOP_ICON = <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-player-stop"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 5m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z" /></svg>
+const SETTINGS_ICON = <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-world-cog"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M21 12a9 9 0 1 0 -8.979 9" /><path d="M3.6 9h16.8" /><path d="M3.6 15h8.9" /><path d="M11.5 3a17 17 0 0 0 0 18" /><path d="M12.5 3a16.992 16.992 0 0 1 2.522 10.376" /><path d="M19.001 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M19.001 15.5v1.5" /><path d="M19.001 21v1.5" /><path d="M22.032 17.25l-1.299 .75" /><path d="M17.27 20l-1.3 .75" /><path d="M15.97 17.25l1.3 .75" /><path d="M20.733 20l1.3 .75" /></svg>
 
 const GetSatatusBadgeClasses = (status: string) => {
     switch (status) {
@@ -38,6 +46,9 @@ const MyServicesContainer = ({
     const [ importDataCurrent, setImportDataCurrent ] = useState<{repositoryNamespace:string, sourceCodeURL:string}>()
     const [ interfaceModeType,  changeMode] = useState<any>(LOADING_MODE)
     const [ provisionedServicesList, setProvisionedServicesList ] = useState([])
+    const [ serviceSettingsData, setServiceSettingsData ] = useState<any>()
+
+    console.log(serviceSettingsData)
 
     useEffect(() => {
 
@@ -82,12 +93,22 @@ const MyServicesContainer = ({
 
     const handleFinishedImportModal = () => changeMode(LOADING_MODE)
 
+    const handleSettingServiceMode = (provisionedServiceData) => {
+        setServiceSettingsData(provisionedServiceData)
+        changeMode(SERVICE_SETINGS_MODE)
+    }
+
     return <>
 
                 <div className="container-xl">
                     <div className="row g-2 align-items-center">
                         <div className="col">
-                            <h2 className="page-title">My Services</h2>
+                            {
+                                interfaceModeType !== SERVICE_SETINGS_MODE
+                                && <h2 className="page-title">My Services</h2>
+                            }
+                            {interfaceModeType === SERVICE_SETINGS_MODE && <div className="page-pretitle">Service Settings</div>}
+                            {interfaceModeType === SERVICE_SETINGS_MODE && <h2 className="page-title">{serviceSettingsData.executableName}</h2>}
                         </div>
                         {
                             interfaceModeType === DEFAULT_MODE
@@ -109,7 +130,12 @@ const MyServicesContainer = ({
                     </div>
                     <div className="py-4">
                         {
+                            interfaceModeType === SERVICE_SETINGS_MODE
+                            && <ServiceSettingsPanel provisionedService={serviceSettingsData}/>
+                        }
+                        {
                             provisionedServicesList 
+                            && interfaceModeType !== SERVICE_SETINGS_MODE
                             && <div className="row">
                                     {
                                         provisionedServicesList.map((provisionedService, index) => (
@@ -119,14 +145,9 @@ const MyServicesContainer = ({
                                                     <div className="card-header py-2">
                                                         <span className={`${GetSatatusBadgeClasses(provisionedService.containerStatus)} me-2`}>{provisionedService.containerStatus}</span>
                                                         <div>
-                                                        <h4 className="card-title">{provisionedService.executableName}</h4>
-                                                        <p className="card-subtitle">{provisionedService.repositoryNamespace}/{provisionedService.packageName}/{provisionedService.packageType}</p>
+                                                            <h4 className="card-title">{provisionedService.executableName}</h4>
+                                                            <p className="card-subtitle">{provisionedService.repositoryNamespace}/{provisionedService.packageName}/{provisionedService.packageType}</p>
                                                         </div>
-                                                        {/*<div className="card-actions">
-                                                            <a className="btn btn-ghost-info p-1">Settings
-                                                            <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-settings ms-1"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" /><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /></svg>
-                                                            </a>
-                                                        </div>*/}
                                                     </div>
                                                     <div className="card-body">
                                                         <dl className="row">
@@ -139,20 +160,23 @@ const MyServicesContainer = ({
                                                             {
                                                                 provisionedService.containerStatus === "exited"
                                                                 && <button className="btn btn-primary" onClick={() => {}}>
-                                                                        <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-player-play"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 4v16l13 -8z" /></svg>start
+                                                                        {START_ICON}start
                                                                     </button>
                                                             }
                                                             {
                                                                 provisionedService.containerStatus === "running"
                                                                 && <>
                                                                         <button className="btn btn-orange">
-                                                                            <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-refresh"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" /><path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" /></svg>restart
+                                                                            {RESTART_ICON}restart
                                                                         </button>
                                                                         <button className="btn btn-danger" onClick={() => {}}>
-                                                                            <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-player-stop"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 5m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z" /></svg>stop
+                                                                            {STOP_ICON}stop
                                                                         </button>
                                                                     </>
                                                             }
+                                                            <button className="btn btn-sencondary" onClick={() => handleSettingServiceMode(provisionedService)}>
+                                                                {SETTINGS_ICON}settings
+                                                            </button>
                                                         </div>
                                                     </div>
                                                 </div>
