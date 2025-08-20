caa0c05f47978a4cd9c71dda03ed80334279f1d5
Kaio Cezar <kadisk.shark@gmail.com>
2025-08-19 22:58:12 -0300

    documentação

--- Website.Module/Application.layer/kadisk-com.webgui/src/Pages/MetaPlatform.page.tsx
+++ Website.Module/Application.layer/kadisk-com.webgui/src/Pages/MetaPlatform.page.tsx
@@ -61,24 +61,90 @@ const MetaPlatformPage = ({
     
             <div className="container-fluid bg-gray-200">
                 <div className="container py-6">
-                    <h2>O MyPlatform é uma meta-framework, ela fornecer uma base sólida de componentes para que frameworks, ecosistemas, aplicações e serviços possam ser construidos de forma rápida e eficiente.</h2>
-                    <p>O Meta Platform Setup Wizard é uma ferramenta de linha de comando feita pra ambientes GNU/Linux usada para configuração e instalação de ecossistemas padrão My Platform.Ele facilita a preparação e personalização da instalação, garantindo que todos os componentes essenciais do ecossistema estejam integrados e funcionando de maneira otimizada.</p>
+                    <h2 className="mb-4">
+                    MyPlatform: uma meta-framework para construção rápida e eficiente
+                    </h2>
+                    <p>
+                    O MyPlatform é uma meta-framework que fornece uma base sólida de componentes, 
+                    permitindo que frameworks, ecossistemas, aplicações e serviços sejam desenvolvidos 
+                    de forma ágil e otimizada.
+                    </p>
                 </div>
             </div>
+
 
             <div className="container-fluid">
                 <div className="container py-6">
-                    <h2>O MyPlatform é uma meta-framework, ela fornecer uma base sólida de componentes para que frameworks, ecosistemas, aplicações e serviços possam ser construidos de forma rápida e eficiente.</h2>
-                    <div className="row">
-                        <div className="col-12">
-                            <p>O Meta Platform Setup Wizard é uma ferramenta de linha de comando feita pra ambientes GNU/Linux usada para configuração e instalação de ecossistemas padrão My Platform.Ele facilita a preparação e personalização da instalação, garantindo que todos os componentes essenciais do ecossistema estejam integrados e funcionando de maneira otimizada.</p>
-                        </div>
+                    <h2 className="mb-4">Repositórios</h2>
+
+                    <div className="mb-5">
+                    <h3 className="mb-2">Meta Platform Essential Repository</h3>
+                    <p>
+                        Este repositório reúne todos os componentes necessários para a execução 
+                        de um ecossistema, além de ferramentas essenciais para sua manutenção.
+                    </p>
+                    </div>
+
+                    <div>
+                    <h3 className="mb-2">Meta Ecosystem Core Repository</h3>
+                    <p>
+                        Contém os componentes relacionados a serviços web e ferramentas de 
+                        suporte e manutenção.
+                    </p>
                     </div>
                 </div>
             </div>
-           
-
-            
+            <div className="container-fluid bg-gray-200">
+                <div className="container py-6">
+                    <h2 className="mb-4">Diferentes tipos de pacotes </h2>
+                   <div className="row items-center">
+                        <div className="col-12">
+                            <div className="space-y-5">
+                                <div>
+                                    <div className="row">
+                                        <div className="col">
+                                            <h3>example-package.lib</h3>
+                                            <p className="text-secondary m-0">São bibliotecas que podem ser usadas por outros pacotes, não é um pacote executável.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="row">
+                                        <div className="col">
+                                            <h3>example-package.webservice</h3>
+                                            <p className="text-secondary m-0">Pacotes que fornece uma interface de comunição em rede de forma configuravel, usado para criar endpoints REST.e um variante especializada do service</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="row">
+                                        <div className="col">
+                                            <h3>example-package.webgui</h3>
+                                            <p className="text-secondary m-0">Pacote que fornece interface de rede e compilação de front-end, e um variante especializada do webservice</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="row">
+                                        <div className="col">
+                                            <h3>example-package.service</h3>
+                                            <p className="text-secondary m-0">Pacotes com serviços que podem ficar executando em segundo plano.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="row">
+                                        <div className="col">
+                                            <h3>example-package.webapp</h3>
+                                            <p className="text-secondary m-0">Esse pacote agrupa todos os outro nele, é usado como o pacote principal geralmente a aplicação principal.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
         </div>
     </>
 }
--- Website.Module/Application.layer/kadisk-com.webgui/src/Pages/MetaPlatformDocumentation.page.tsx
+++ Website.Module/Application.layer/kadisk-com.webgui/src/Pages/MetaPlatformDocumentation.page.tsx
@@ -35,9 +35,37 @@ const MetaPlatformDocumentationPage = ({
 						<div className="collapse navbar-collapse">
 							<ul className="navbar-nav">
 								<li className="nav-item">
-									<a className="nav-link">
+									<a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="false" role="button">
 										<span className="nav-link-title">Primeiros passos</span>
 									</a>
+									<div className="dropdown-menu show">
+										<div className="dropdown-menu-columns">
+											<div className="dropdown-menu-column">
+												<a className="dropdown-item">Configurando um ecosistema localmente</a>
+											</div>
+										</div>
+									</div>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link">
+										<span className="nav-link-title">Arquitetura Ecosistema</span>
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="false" role="button">
+										<span className="nav-link-title">Pacotes</span>
+									</a>
+									<div className="dropdown-menu show">
+										<div className="dropdown-menu-columns">
+											<div className="dropdown-menu-column">
+												<a className="dropdown-item">lib</a>
+												<a className="dropdown-item">service</a>
+												<a className="dropdown-item">webservice</a>
+												<a className="dropdown-item">webgui</a>
+												<a className="dropdown-item">webapp</a>
+											</div>
+										</div>
+									</div>
 								</li>
 							</ul>
 						</div>
@@ -49,51 +77,108 @@ const MetaPlatformDocumentationPage = ({
 							<div className="row g-2 align-items-center">
 								<div className="col">
 									<div className="page-pretitle">Primeiros Passos</div>
-									<h2 className="page-title">Instalando um ecosistema localmente</h2>
+									<h2 className="page-title">Configurando um ecosistema localmente</h2>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div className="page-body">
 						<div className="container-xl">
-							<div className="row items-center">
+							<div className="row">
+								<div className="col">
+									<h3>Executor de Pacotes</h3>
+									<p>Para executar os ecossitema e seus pacotes é necessario que o packace-executor(<a href="https://github.com/Meta-Platform/meta-platform-package-executor-command-line">Executor de Pacotes da Plataforma</a>) esteja instalado em seu ambiente de execução e o comando <code>pkg-exec</code>.</p>
+									<pre>
+										<code>
+											npm install -g package-executor
+										</code>
+									</pre>
+									<pre>
+										<code>
+											pkg-exec
+										</code>
+									</pre>
+									<pre>{`
+Opções:
+      --version                             Exibe a versão                                                                       [booleano]
+      --package                             Caminho do pacote                                                                    [string] [obrigatório]
+      --startupJson                         Caminho do arquivo JSON com os parâmetros de inicialização                           [string] [obrigatório]
+      --ecosystemData                       Caminho do diretório EcosystemData válido                                            [string] [obrigatório]
+      --ecosystemDefault                    Caminho do arquivo JSON de parâmetros da plataforma                                  [string] [obrigatório]
+      --nodejsProjectDependencies           Caminho do projeto com as dependências nodejs para execução do pacote do ecossistema [string] [obrigatório]
+      --awaitFirstConnectionWithLogStreami  Aguardar pela primeira conexão com ng streaming de logs                              [booleano] [padrão: false]
+      --supervisorSocket                    Caminho onde será criado o socket de supervisão do processo executor de pacotes      [string] [padrão: false]
+      --executableName                      Nome do executável para pacote de linha de comando                                   [string] [padrão: false]
+      --commandLineArgs                     Argumentos para o executable de linha de comando                                     [string] [padrão: false]
+      --verbose                             Aguardar pela primeira conexão com streaming de logs                                 [booleano] [padrão: false]
+  -h, --help                                Exibe ajuda                                                                          [booleano]
+
+Faltando argumentos obrigatórios: package, startupJson, ecosystemData, ecosystemDefault, nodejsProjectDependencies
+									`}</pre>
+								</div>
+							</div>
+							<div className="row">
 								<div className="col-12">
-									<div className="space-y-5">
-										<div>
-											<div className="row">
-												<div className="col">
-													<p>Atualmente, a forma mais rápida de instalar um ecossistema é utilizando a ferramenta oficial de linha de comando <a href="https://github.com/Meta-Platform/meta-platform-setup-wizard-command-line">Meta Platform Setup Wizard</a>. Desenvolvida para ambientes GNU/Linux, ela permite configurar e instalar ecossistemas padrão My Platform de maneira simples. A ferramenta facilita a preparação e personalização da instalação, garantindo que todos os componentes essenciais estejam integrados e funcionando de forma otimizada.</p>
-													<h3>Obtendo o Setup Wizard</h3>
-													<p>Para obter a ferramenta, você pode baixá-la diretamente do repositório oficial no GitHub. Use o comando abaixo para fazer o download da versão mais recente:</p>
-													<div>
-														<pre>
-															<code>
-																wget https://github.com/Meta-Platform/meta-platform-setup-wizard-command-line/releases/download/0.0.19/meta-platform-setup-wizard-command-line-0.0.19-preview-linux-x64 -O mywizard
-															</code>
-														</pre>
-													</div>
-													<p className="text-secondary">Certifique-se de ter o <code>wget</code> instalado no seu sistema. Caso não queira <a href="https://github.com/Meta-Platform/meta-platform-setup-wizard-command-line/releases/download/0.0.19/meta-platform-setup-wizard-command-line-0.0.19-preview-linux-x64">clique aqui</a> para baixar manualmente.</p>
-													<p>Após o download, você precisará tornar o arquivo executável. Use o comando <code>chmod +x</code> para isso:</p>
-													<div>
-														<pre>
-															<code>
-																chmod +x mywizard
-															</code>
-														</pre>
-													</div>
-													<p>Agora, você pode executar o Setup Wizard diretamente do terminal. Para listar os perfis de ecossistemas disponíveis, utilize o seguinte comando:</p>
-													<h4>Exemplo de comandos</h4>
-													<div>
-														<pre>
-															<code>
-																./mywizard list-profiles
-															</code>
-														</pre>
-													</div>
+									<div>
+										<div className="row">
+											<div className="col">
+												<h3>Setup Wizard</h3>
+												<p>Atualmente, a forma mais rápida de instalar um ecossistema é utilizando a ferramenta oficial de linha de comando mywizard <a href="https://github.com/Meta-Platform/meta-platform-setup-wizard-command-line">Meta Platform Setup Wizard</a>.
+Desenvolvida para ambientes GNU/Linux, ela permite configurar e instalar ecossistemas padrão My Platform de maneira simples. A ferramenta facilita a preparação e personalização da instalação, garantindo que todos os componentes essenciais estejam integrados e funcionando de forma otimizada.</p>
+												<div>
+													<pre>
+														<code>
+															npx mywizard --help
+														</code>
+													</pre>
+													<pre>{`
+mywizard [comando]
+
+Comandos:
+mywizard list-profiles             Exibe os perfis de instalação disponíveis
+mywizard install [profile]         Instala um ecosistema conforme o [installation-path] perfil especificado
+mywizard update [profile]          Atualiza os repositórios, [installation-path] executáveis e binários de um ecosistema instalado
+mywizard show-profile [profile]    Mostra informações sobre um perfil especifico
+
+Opções:
+--help     Exibe ajuda			[booleano]
+--version  Exibe a versão		[booleano]
+												`}</pre>
+												</div>
+												
+												<p>Para instalar um ecosistema utilizando o perfil de instalação <i>standard</i> por padrão</p>
+												<div>
+													<pre>
+														<code>
+															mywizard install release-standard
+														</code>
+													</pre>
+													<p className="text-secondary">Certifique-se de ter o <code>mywizard</code> instalado no seu sistema ou use <code>npx mywizard ...</code>. Tambem exista a opção de um binário equivalente <a href="https://github.com/Meta-Platform/meta-platform-setup-wizard-command-line/releases/download/0.0.19/meta-platform-setup-wizard-command-line-0.0.19-preview-linux-x64">aqui</a> para ou <code>wget https://github.com/Meta-Platform/meta-platform-setup-wizard-command-line/releases/download/0.0.19/meta-platform-setup-wizard-command-line-0.0.19-preview-linux-x64 -O mywizard</code>.</p>
 												</div>
 											</div>
 										</div>
 									</div>
+								</div>
+							</div>
+							<div className="row">
+								<div className="col">
+									<h3>Configurando o PATH no .bashrc</h3>
+									<p>Para que o sistema reconheça os comandos e executáveis do ecossistema, é necessário adicionar o diretório EcosystemData à variável de ambiente PATH. Siga os passos abaixo:</p>
+									<pre>
+										<code>
+											{`echo 'export PATH="$PATH:~/EcosystemData/executables"' >> ~/.bashrc && source ~/.bashrc`}
+										</code>
+									</pre>
+									Verifique com
+									<pre>
+										<code>
+											repo list installed
+										</code>
+{`
+	1. EssentialRepo
+	2. EcosystemCoreRepo
+`}
+									</pre>
 								</div>
 							</div>
 						</div>
