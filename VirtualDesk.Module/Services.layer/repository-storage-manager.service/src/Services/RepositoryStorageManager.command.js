14fb411b28394695be24110fca71ed682ce1c7b0
Kaio Cezar <kadisk.shark@gmail.com>
2025-09-04 10:25:02 -0300

    base repository-storage-manager.service

---
+++ VirtualDesk.Module/Services.layer/repository-storage-manager.service/src/Services/RepositoryStorageManager.command.js
@@ -0,0 +1,17 @@
+
+
+const RepositoryStorageManagerService = (params) => {
+
+const {
+
+        onReady 
+    } = params
+
+    onReady()
+    
+
+
+    return {}
+}
+
+module.exports = RepositoryStorageManagerService
