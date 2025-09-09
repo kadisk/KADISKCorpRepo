1689b9e9d3462f4233688f85f05b50f64ea9d176
Kaio Cezar <kadisk.shark@gmail.com>
2025-09-09 11:52:28 -0300

    ServiceManagerInterface

---
+++ VirtualDesk.Module/Applications.layer/my-services-manager.app/src/Controllers/ServiceManagerInterface.controller.js
@@ -0,0 +1,84 @@
+const ServiceManagerInterfaceController = (params) => {
+
+    const {
+        myServicesManagerService
+    } = params
+
+
+    const {
+        ListProvisionedServices,
+        GetServiceData,
+        GetNetworksSettings,
+        ListImageBuildHistory,
+        ListInstances,
+        ListContainers,
+        GetServiceStatus,
+        StartService,
+        StopService,
+        GetInstanceStartupParamsData,
+        GetInstanceStartupParamsSchema,
+        GetInstancePortsData,
+        GetNetworkModeData,
+        UpdateServicePorts,
+        UpdateServiceStartupParams,
+        ProvisionService
+    } = myServicesManagerService
+
+
+    const ServicesStatusChange = async (websocket) => {
+        myServicesManagerService
+            .onChangeServiceStatus(({ serviceId, status }) => {
+                websocket.send(JSON.stringify({ serviceId, status }))
+            })
+    }
+
+    const InstanceListChange = async (websocket, serviceId) => {
+        myServicesManagerService
+            .onChangeInstanceListData(serviceId, (instanceList) => {
+                websocket.send(JSON.stringify(instanceList))
+            })
+    }
+
+    const ContainerListChange = async (websocket, serviceId) => {
+        myServicesManagerService
+            .onChangeContainerListData(serviceId, (containerList) => {
+                websocket.send(JSON.stringify(containerList))
+            })
+    }
+
+    const ImageBuildHistoryListChange = async (websocket, serviceId) => {
+        myServicesManagerService
+            .onChangeImageBuildHistoryListData(serviceId, (imageBuildHistoryList) => {
+                websocket.send(JSON.stringify(imageBuildHistoryList))
+            })
+    }
+
+
+    const controllerServiceObject = {
+        controllerName: "ServiceManagerInterfaceController",
+        ListProvisionedServices,
+        GetServiceData,
+        GetNetworksSettings,
+        ListImageBuildHistory,
+        ListInstances,
+        ListContainers,
+        ServicesStatusChange,
+        InstanceListChange,
+        ContainerListChange,
+        ImageBuildHistoryListChange,
+        GetServiceStatus,
+        StartService,
+        StopService,
+        GetInstanceStartupParamsData,
+        GetInstanceStartupParamsSchema,
+        GetInstancePortsData,
+        GetNetworkModeData,
+        UpdateServicePorts,
+        UpdateServiceStartupParams,
+        ProvisionService
+    }
+
+    return Object.freeze(controllerServiceObject)
+}
+
+module.exports = ServiceManagerInterfaceController
